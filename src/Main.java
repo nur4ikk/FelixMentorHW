b35d67ba40b4e5257429bf3d3e3a1273b3d02b1b
nur4ikk <nursultankurmanaliev25@gmail.com>
2025-03-25 04:41:33 +0000

    My First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
 import java.util.ArrayList;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
         ArrayList<String> listUni = new ArrayList<>();
 
-        //Добавляем элементы в конец списка
+        //Добавляем элементы в ArrayList
         listUni.add("MIT");
         listUni.add("Stanford");
         listUni.add("Harvard");
-        listUni.add("Oxford");
+        listUni.add("Caltech");
 
-        System.out.println(listUni);
+        //Добавляем элементы по указанному индексу
+        listUni.add(2,"Oxford");
+        listUni.add(3,"Cambridge");
 
 
     }
