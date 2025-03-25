557697879cc35d39a3796204286be39cc41efa92
nur4ikk <nursultankurmanaliev25@gmail.com>
2025-03-25 04:20:22 +0000

    My First Commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Main{
+    public static void main(String[] args) {
+        ArrayList<String> listUni = new ArrayList<>();
+
+        //Добавляем элементы в конец списка
+        listUni.add("MIT");
+        listUni.add("Stanford");
+        listUni.add("Harvard");
+        listUni.add("Oxford");
+
+        System.out.println(listUni);
+
+
+    }
+}
